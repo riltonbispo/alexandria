380027889f2989c4065926d8e872123cf39a3e44
riltonbispo <riltondev@gmail.com>
2023-12-20 17:10:03 -0300

    feat: create header with toggle theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
+import { ToggleTheme } from '@/components/toggleTheme'
+import { Input } from '@/components/ui/input'
+
 export default function Home() {
   return (
-    <main>
-      <h1>Home</h1>
-    </main>
+    <div className="container">
+      <header className="flex justify-center items-center gap-10 my-8">
+        <Input
+          type="text"
+          placeholder="pesquise seu livro favorito"
+          className="max-w-lg"
+        />
+        <ToggleTheme />
+      </header>
+    </div>
   )
 }
